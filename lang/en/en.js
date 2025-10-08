7762c1a8611f959d4865bf7781098cc7457a638b
izcheung <icheung24@my.bcit.ca>
2025-10-07 18:20:17 -0700

    feat: add messages

---
+++ lang/en/en.js
@@ -0,0 +1,8 @@
+module.exports = {
+  doesNotExist: "%1 does not exist",
+  alreadyExists: "Warning! '%1' already exists.",
+  numberRequest: "Request # %1.  Total entries: %2",
+  wrongPath: "Wrong path",
+  methodNotAllowed: "Method must be GET or POST",
+  emptyInput: "Empty word is not allowed",
+};
